7198667c15c470b3ce3e6e12e2e3d63c58894881
Jacob Simpson <simpson.jacob.barry@gmail.com>
2020-11-16 11:47:46 -0700

    Firestore core setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:maintenance_man_v2/custom_components/custom_color_theme.dart';
 import 'package:maintenance_man_v2/providers/vehicles.dart';
@@ -5,30 +6,41 @@ import 'package:maintenance_man_v2/providers/vehicles.dart';
 import 'package:maintenance_man_v2/screens/vehicle_records_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 // Themes available at: https://coolors.co/404e5c-4f6272-f8f1ff-57755b-7bd389
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => Vehicles(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Maintenance Man',
-        theme: ThemeData(
-          primarySwatch: CustomColorTheme().customColor(),
-          // primaryColorDark: Color.fromRGBO(64, 78, 92, 1),
-          // primaryColorLight: Color.fromRGBO(79, 98, 114, 1),
-          // accentColor: Color.fromRGBO(123, 211, 137, 1),
-          // backgroundColor: Color.fromRGBO(248, 241, 255, 1),
-        ),
-        home: VehicleRecordsScreen(),
-        routes: {},
-      ),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (ctx, snapshot) =>
+          snapshot.connectionState == ConnectionState.waiting
+              ? CircularProgressIndicator()
+              : MultiProvider(
+                  providers: [
+                    ChangeNotifierProvider(
+                      create: (ctx) => Vehicles(),
+                    ),
+                  ],
+                  child: MaterialApp(
+                    title: 'Maintenance Man',
+                    theme: ThemeData(
+                      primarySwatch: CustomColorTheme().customColor(),
+                      // primaryColorDark: Color.fromRGBO(64, 78, 92, 1),
+                      // primaryColorLight: Color.fromRGBO(79, 98, 114, 1),
+                      // accentColor: Color.fromRGBO(123, 211, 137, 1),
+                      // backgroundColor: Color.fromRGBO(248, 241, 255, 1),
+                    ),
+                    home: VehicleRecordsScreen(),
+                    routes: {},
+                  ),
+                ),
     );
   }
 }
